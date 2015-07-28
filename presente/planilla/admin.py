6648ab59ec3e3ce49247002e36dcf6d271084cb2
Carlos Mathiasen <carlos@blueguazu.com>
2015-07-28 20:43:53 -0300

    Minor

--- presente/planilla/admin.py
+++ presente/planilla/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from planilla.models import Planilla
 
 # Register your models here.
-admin.site.register(Planilla)
+# admin.site.register(Planilla)
--- presente/universidad/models.py
+++ presente/universidad/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from empleados.models import Profesor, Decano, Secretario, Recepcionista, CoordinadorCarrera
-from curso.models import Curso, CursoPorAlumno
+# from curso.models import Curso
 
 class Facultad(models.Model):
   decano = models.ForeignKey(Decano)
@@ -57,11 +57,11 @@ class Materia(models.Model):
     return self.nombre
 
 
-class Planilla(models.Model):
-  curso = models.ForeignKey(Curso)
-  semestre = models.ForeignKey(Semestre)
-  created_at = models.DateField(auto_now_add=True)
-  updated_at = models.DateField(auto_now=True) 
+# class Planilla(models.Model):
+#   curso = models.ForeignKey(Curso)
+#   semestre = models.ForeignKey(Semestre)
+#   created_at = models.DateField(auto_now_add=True)
+#   updated_at = models.DateField(auto_now=True) 
 
-  def __str__(self):
-    return self.curso
+#   def __str__(self):
+#     return self.curso
